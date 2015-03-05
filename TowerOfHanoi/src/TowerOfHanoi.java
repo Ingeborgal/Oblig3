2c58086235cd2f380055d6bde9ed278d22c66199
Ingeborgal <Ingis_al@msn.com>
2015-03-05 12:52:03 +0100

    Version 0.3

--- TowerOfHanoi/src/TowerOfHanoi.java
+++ TowerOfHanoi/src/TowerOfHanoi.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class TowerOfHanoi {
 
 	static int teller = 0;
+	static int rekursivTeller = 0;
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
@@ -13,6 +14,7 @@ public class TowerOfHanoi {
 		System.out.println("Trekkene er: ");
 		flyttDisker(n, 'A', 'B', 'C');
 		System.out.println("Antall flyttninger er: " + teller );
+		System.out.println("Antall kall til rekursiv metode er: " + rekursivTeller);
 	}
 
 	private static void flyttDisker(int n, char fraTaarn, char tilTaarn, char auxTaarn) {
@@ -24,7 +26,7 @@ public class TowerOfHanoi {
 			flyttDisker(n-1, fraTaarn, auxTaarn, tilTaarn);
 			System.out.println("flytt disk " + n + " fra " + fraTaarn + " til " + tilTaarn);
 			flyttDisker(n -1, auxTaarn, tilTaarn, fraTaarn);
-			
+			rekursivTeller++;
 			
 		}
 		
