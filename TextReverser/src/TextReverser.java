6270dfad6118d0132d38ea2e05f7898e7f7f8a0c
Ingeborgal <Ingis_al@msn.com>
2015-03-05 13:37:18 +0100

    Version0.2

--- TextReverser/src/TextReverser.java
+++ TextReverser/src/TextReverser.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class TextReverser {
 
+	static int teller;
 	public static void main(String[] args) {
 		
 		System.out.println("Skriv et ord: ");
@@ -11,15 +12,20 @@ public class TextReverser {
 		
 		reverse(ord);
 		
+		teller = ord.length();
+		
 		System.out.println("Ordet reversert er: " + reverse(ord));
 		
+		System.out.println("Antall bokstaver i ordet: " + teller);
 
 	}
 
 	public static String reverse(String text){
+		
 		if((null == text) || (text.length() <= 1)){
 			return text;
 		}
+		
 		return reverse(text.substring(1)) + text.charAt(0);
 	}
 }
